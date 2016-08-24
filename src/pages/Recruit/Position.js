a99d3a224a1121124c296f3ceb9677b229097279
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-08-24 18:55:23 +0800

    [#10] - Added role description for position.

--- src/pages/Recruit/Position.js
+++ src/pages/Recruit/Position.js
@@ -1,12 +1,85 @@
 import React from 'react'
 
 import NavSubTitle from '../../components/NavSubTitle'
-import './Schedule.less'
+import './Position.less'
 
 const Position = () => (
   <div className="container">
     <NavSubTitle title="软件开发／测试工程师"/>
+    <p className="roles">
+      <div className="role-description"><strong>工作地点：</strong></div>
+      <span className="address">北京、西安、成都、武汉</span>
+      <div className="role-description"><strong>ThoughtWorks需要你</strong></div>
+      <ul>
+        <li>学习能力非常强</li>
+        <li>非常的热爱技术，是真正的舍得花时间的那种热爱</li>
+        <li>喜欢讨论技术，分享技术</li>
+        <li>重视对于团队和客户的承诺</li>
+        <li>希望未来的几年里挑战不同的技术，扩大视野</li>
+      </ul>
+      <div className="role-description"><strong>同时我们不需要你</strong></div>
+      <ul>
+        <li>毕业于顶级知名高校</li>
+        <li>自带流利的英文，甚至普通话</li>
+        <li>一定是计算机相关专业毕业，做过很多项目</li>
+      </ul>
+      <div className="role-description"><strong>加入ThoughtWorks是一种怎样的体验？</strong></div>
+      <ul>
+        <li>全球化，最前沿的技术资源，不同国家之间没有技术壁垒</li>
+        <li>入职五周的公派留学，未来参与公司平均“10%比例”的海外出差项目</li>
+        <li>跟世界上最活跃的程序员一起工作，学习，编程，分享，接受挑战</li>
+        <li>为全球各行业最顶尖的客户、最重要的IT项目提供专业服务</li>
+      </ul>
+      <div className="role-description"><strong>那么你需要付出什么？</strong></div>
+      <ul>
+        <li>更多的时间去学习，实践技术</li>
+        <li>你需要适应出差。国内国外的出差都有可能</li>
+        <li>你需要更主动去了解你喜欢的技术和项目，快速成长</li>
+        <li>你需要建立自己的影响力，在这样一个扁平而自由的组织里，我们需要你精彩的发声</li>
+      </ul>
+    </p>
+
     <NavSubTitle title="技术领导管培生"/>
+    <p className="roles">
+      <div className="role-description"><strong>工作地点：</strong></div>
+      <span className="address">北京、西安、成都、武汉</span>
+      <div className="role-description"><strong>什么是管理培训生计划？</strong></div>
+      <p>管培生计划指公司招聘具有最高相关潜力的毕业生，作为未来的管理层预备队，并为他们特别打造的小而精的系统性培养计划。计划的实施，通常由公司最高水准的相关领导者直接负责，通过整合各种公司资源，帮助其有计划有针对性的轮岗，完成对公司业务的全面了解。同时，会挖掘其深度和视野，帮助其最快速成长承担重要的责任。
+      </p>
+      <p>由于管理委培会占用公司大量的资源，通常会有更严格的考核和退出机制，在薪资上也会做出相应的补充</p>
+      <div className="role-description"><strong>ThoughtWorks的技术管培生是一种什么样的体验？</strong></div>
+      ThoughtWorks招聘的技术领导管培生：与其他公司的管培生略有不同，我们要培养技术领导者，即一家信息技术服务公司的：Solution Architect（解决方案架构师）, Product Manager（产品经理）, Account Tech
+      Principal（客户技术领导者）。主要能力包括：
+      <ul>
+        <li>全面深入的技术能力</li>
+        <li>管理技巧、变革管理能力</li>
+        <li>出色的客户沟通能力</li>
+      </ul>
+      因此，此职位由中国区首席技术官（CTO）徐昊直接负责培养，通过提供有计划有针对性的轮岗，帮助完成对公司业务的全面了解，并培养足够深度和广度的技能技巧，为未来工作做好准备。
+      <div className="role-description"><strong>为什么要加入ThoughtWorks管培生计划？</strong></div>
+      因为，在未来两年，你将通过努力获得：
+      <ol>
+        <li>中国区首席技术官对你职业发展的全面及亲自辅导，他也将根据你发展的需要而安排项目和专项导师。</li>
+        <li>彻底了解咨询公司的各种业务上下文，最佳实践和运营方式</li>
+        <li>广泛的技术视野和深厚的实践经验，及设计方案和驱动变革的能力</li>
+        <li>接触和对话各大公司中高层技术及其他管理者的机会和能力</li>
+        <li>更具有竞争力的薪酬</li>
+      </ol>
+      <div className="role-description"><strong>我们期望你是</strong></div>
+      <ul>
+        <li>非常热爱技术，有扎实的编程基础，有一些小雏形产品可以秀一下就更好咯</li>
+        <li>学习能力非常强，非常欢迎自学成才的你</li>
+        <li>会花很多时间写代码、看技术书籍</li>
+        <li>喜欢讨论技术，分享技术</li>
+        <li>重视对于团队合作</li>
+        <li>有扩大视野的渴望，希望未来的几年里挑战不同的技术</li>
+      </ul>
+      <div className="role-description"><strong>同样，在这特别的机遇背后，也面临着严峻的挑战：</strong></div>
+      <ol>
+        <li>每月一次的综合反馈、每季度一次的表现评估，三次不达标，则遗憾的说：你需要离开ThoughtWorks</li>
+        <li>80%时间，你在各地出差，或在云端飞行</li>
+      </ol>
+    </p>
   </div>
 )
 
