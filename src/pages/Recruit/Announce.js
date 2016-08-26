518878d87ed8977cc08dda1be206338b48a75d8a
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-08-26 12:08:25 +0800

    [#10] - Add Announce and change NavSubTitle

--- src/pages/Recruit/Announce.js
+++ src/pages/Recruit/Announce.js
@@ -2,10 +2,21 @@ import React from 'react'
 
 import NavSubTitle from '../../components/NavSubTitle'
 
-const Position = () => (
-  <div className="container">
+import './Announce.less'
+
+const Announce = () => (
+  <div className="container Announce">
     <NavSubTitle title="校招动态"/>
+    <p>
+      <div className="sub-title">ThoughtWorks 2017校园招聘全面启动！</div>
+      <ol>
+        <li><strong>面向人群</strong>：2017应届毕业生</li>
+        <li><strong>招聘职位</strong>：软件开发工程师、软件测试工程师、技术领导管培生</li>
+        <li><strong>应聘方式</strong>：登陆 <a href="http://join.thoughtworks.cn">http://join.thoughtworks.cn</a> 注册并投递简历！</li>
+      </ol>
+    </p>
+
   </div>
 )
 
-export default Position
+export default Announce
