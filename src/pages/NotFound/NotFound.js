21a87669de70729763adc0f69ab77ba677af733e
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-09-08 00:37:54 +0800

    [#12] - Moved NotFound page into own folder.

---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './NotFound.css'
+
+const NotFound = () => (
+  <div>
+    <a href="/">
+      <img src={require('../../../images/TW Colour Logos_pink.png')} className="img-responsive center-block" alt="Logo"/>
+      <br/>
+      <br/>
+      <br/>
+      <img src={require('../../../images/social-globe.png')} className="App-logo img-responsive center-block" alt="ThoughtWorks"/>
+    </a>
+  </div>
+)
+
+export default NotFound
