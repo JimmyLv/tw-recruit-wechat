fa887388e0a6d80b7b6bd9bfd3474046dda74fb6
linesh <linesh.simpcity@gmail.com>
2016-09-13 19:21:13 +0800

    [#16] Fix styling deploying to prod

--- src/components/GameHeader.js
+++ src/components/GameHeader.js
@@ -3,7 +3,7 @@ import logo from '../../images/game/header/cloud.png'
 
 const Header = () => (
     <div className="app-header">
-        <img src={logo} alt="header-cloud" className="img-responsive"/>
+        <img src={logo} alt="header-cloud" className="img-responsive header-cloud"/>
     </div>
 )
 
