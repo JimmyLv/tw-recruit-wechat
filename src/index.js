7e4115ff06bb7d53c7182d01d430aeacd462b870
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-09-13 17:10:42 +0800

    [#16] - Changed back browserHistory to hashHistory due to not work in GitHub Page.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import Main from './pages/Main'
 import Recruit from './pages/Recruit'
@@ -20,7 +20,7 @@ import NotFound from './pages/NotFound'
 import './index.css'
 
 ReactDOM.render((
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path="/">
         <IndexRoute component={Main}/>
         <Route path="recruit">
